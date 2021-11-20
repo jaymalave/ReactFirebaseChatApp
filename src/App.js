5f84c773f5b23a7cb7b72ed9943a797fae56ad0b
jaymalave <jaym.github@gmail.com>
2021-11-20 19:34:00 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-center fw-bold">React firebase chat app</h1>
+    </div>
+  );
+}
+
+export default App;
