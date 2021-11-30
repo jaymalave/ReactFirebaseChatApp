e5a5e645e5412a16bdb3bd224560cfc9e5677709
jaymalave <jaym.github@gmail.com>
2021-12-01 01:47:58 +0530

    add Ref for auto scroll to bottom

--- src/components/ChatRoom/ChatRoom.js
+++ src/components/ChatRoom/ChatRoom.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import firebase from "firebase/compat/app";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import ChatMessage from "../ChatMessage/ChatMessage";
 import SendMessage from "../SendMessage/SendMessage";
 import SignOut from "../SignOut/SignOut";
 
+
+
 const ChatRoom = () => {
+  const dummy = useRef();
   const firestore = firebase.firestore();
   const messagesRef = firestore.collection("messages");
   const query = messagesRef.orderBy("createdAt").limitToLast(25);
 
   const [messages] = useCollectionData(query, { idField: "id" });
+
+  useEffect(() => {
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  });
 
   return (
     <>
@@ -20,8 +27,9 @@ const ChatRoom = () => {
             <ChatMessage key={message.id} message={message} />
           </div>
         ))}
-        <SendMessage/>
-        <SignOut/>
+      <span ref={dummy}></span>
+      <SendMessage />
+      <SignOut />
     </>
   );
 };
